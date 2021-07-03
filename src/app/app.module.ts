88a43be269ca74bbf0fb875b4c9b85c2af9be26b
Avinash Wadhawan <avinashxw@gmail.com>
2021-07-03 23:53:11 +0530

    level-1-routing-done-attendance-dashboard-leaves-page404-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AttendanceComponent } from './attendance/attendance.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LeavesComponent } from './leaves/leaves.component';
+import { Page404Component } from './page404/page404.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AttendanceComponent
+    AttendanceComponent,
+    DashboardComponent,
+    LeavesComponent,
+    Page404Component,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
