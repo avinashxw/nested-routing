625e7d9c7c0216bb4bae918b8d58b30654b1eb2f
Avinash Wadhawan <avinashxw@gmail.com>
2021-07-03 23:47:22 +0530

    attendance-component-added

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AttendanceComponent } from './attendance/attendance.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AttendanceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
